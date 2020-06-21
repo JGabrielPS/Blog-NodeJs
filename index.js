e9d8829d319cc064362b1970b78875a4b073916f
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2020-06-21 01:01:19 -0300

    Subject: Motores de Modelado
    Issue N°: 5
    
    Description: Se usa el motor de modelado EJS para dinamizar los elementos head, navbar, footer y scripts de las paginas. Se corrigen las rutas en el index.js y se cambian la extension de los archivos html correspondientes. Se crea la carpeta layouts para guardar los elementos dinamicos

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express");
-const app = new express();
 const path = require("path");
+const ejs = require("ejs");
+
+const app = new express();
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
 });
 
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
 });
 
 app.listen(4000, () => {
