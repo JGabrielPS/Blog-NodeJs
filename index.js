8d35b869a65b3651c4a71c483131439fc2a52f29
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2020-06-23 00:30:05 -0300

    Subject: Agregando la pagina para crear nuevos post
    Issue N°: 7
    
    Description: Se agrego el archivo create, donde se crearon los nuevos post para el blog, ademas se cambiaron las rutas de acceso a los css y scripts para que sean absolutas asi no hay errores a la hora de accederlos. Tambien se agrego el middleware body-parser ya que en el index se agregaron los metodos get y post de la pagina create

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
 
 const app = new express();
 
 app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -25,6 +29,15 @@ app.get("/contact", (req, res) => {
 
 app.get("/post", (req, res) => {
   res.render("post");
+});
+
+app.get("/post/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/post/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen(4000, () => {
