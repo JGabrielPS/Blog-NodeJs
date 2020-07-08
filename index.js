e8b5cd5ca7187e8f097f48d4485914762f3e42fc
Juan Gabriel Perez Salvatierra <juan.gabriel.ps@gmail.com>
2020-07-08 13:05:04 -0300

    Subject: Introduccion a los middleware de express
    Issue N°: 11
    
    Description: Se agrego el middleware de validacion para la creacion de nuevos post en index.js. Se valida el endpoint /post/store, ademas se agrego una nueva imagen del post de prueba

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.use(fileUpload());
 
 app.set("view engine", "ejs");
 
+const validateMiddelWare = (req, res, next) => {
+  if (req.files === null || req.body.title === '' || req.body.body === '') {
+    return res.redirect("/post/new");
+  }
+  next();
+};
+
+app.use("/post/store", validateMiddelWare);
+
 app.get("/", async (req, res) => {
   const blogposts = await BlogPost.find({});
   res.render("index", {
@@ -57,7 +66,7 @@ app.get("/post/new", (req, res) => {
 app.post("/post/store", (req, res) => {
   let image = req.files.image;
   image.mv(path.resolve(__dirname, "public/img", image.name), async (error) => {
-    await BlogPost.create({...req.body, image:'/img/' + image.name});
+    await BlogPost.create({ ...req.body, image: "/img/" + image.name });
     res.redirect("/");
   });
 });
